b158cd7ba753b599fdb2c42526c1e49253b47ab1
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-02-13 10:59:36 -0300

    Lista01-questao4

---
+++ resolucaoListasJavaPOO/src/lista01/questao4/ValoresMonetarios.java
@@ -0,0 +1,29 @@
+package lista01.questao4;
+
+import java.util.Scanner;
+
+public class ValoresMonetarios {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float valorUm;
+		float valorDois;
+		float porcentagem;
+		
+		System.out.println("Informe o primeiro valor:");
+		valorUm = sc.nextFloat();
+		
+		System.out.println("Informe o segundo valor:");
+		valorDois = sc.nextFloat();
+		
+		if(valorUm > valorDois)
+			porcentagem = (valorDois * 100)/valorUm;
+		else
+			porcentagem = (valorUm * 100)/valorDois;
+		
+		System.out.println("O menor valor representa " + porcentagem + "% do maior valor");
+		
+		sc.close();
+	}
+
+}
