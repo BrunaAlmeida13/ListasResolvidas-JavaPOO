51956047142b0cf0b8c60eced352b0309a82796b
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-02-18 10:57:58 -0300

    incluido_questao7

---
+++ resolucaoListasJavaPOO/src/lista01/questao7/ConversorTemperatura.java
@@ -0,0 +1,27 @@
+package lista01.questao7;
+
+import java.util.Scanner;
+
+public class ConversorTemperatura {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double cel;
+		double far;
+		
+		System.out.print("Escreva a temperatura em graus Celsius: ° ");
+		cel = sc.nextFloat();
+		fahrnheit(cel);
+		
+		far = fahrnheit(cel);
+		
+		System.out.printf("A temperatura em Fahrenheit, é: %.2f°F.", far);
+		
+		sc.close();
+	}
+	
+	public static double fahrnheit(double cel) {
+		double far = (9 * cel + 160) / 5;
+		return far;
+	}
+}
