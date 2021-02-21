50895b7622d11ce49e0dfed68db570ded274b8ee
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-02-21 08:58:15 -0300

    lista01.questao9_finalizada

--- resolucaoListasJavaPOO/src/lista01/questao9/Main.java
+++ resolucaoListasJavaPOO/src/lista01/questao9/Main.java
@@ -9,12 +9,40 @@ public class Main {
 		MyMath mm = new MyMath();
 		int calcFatorial;
 		int fatorial;
+		float valorUm;
+		float valorDois;
+		int valorPar;
+		boolean verifica;
+		int valorPrimo;
 		
 		System.out.println("Digite um número inteiro para calcular seu fatorial");
 		calcFatorial = sc.nextInt();
-		
 		fatorial = mm.calculaFatorial(calcFatorial);
 		System.out.println("O fatorial de " + calcFatorial + "! é: " + fatorial);
+		
+		System.out.println("----------------------------------------------------");
+		
+		System.out.println("Descobrindo o maior número...");
+		System.out.println("Digite o primeiro valor:");
+		valorUm = sc.nextFloat();
+		System.out.println("Digite o segundo valor:");
+		valorDois = sc.nextFloat();
+		mm.maiorValor(valorUm, valorDois);
+		
+		System.out.println("----------------------------------------------------");
+		
+		System.out.println("Descobrindo se o número é par...");
+		System.out.println("Digite um valor inteiro positivo:");
+		valorPar = sc.nextInt();
+		verifica= mm.verificaPar(valorPar);
+		System.out.println("Par? " + verifica);
+		
+		System.out.println("----------------------------------------------------");
+		
+		System.out.println("Descobrindo se o número é primo...");
+		System.out.println("Digite um valor inteiro positivo:");
+		valorPrimo = sc.nextInt();
+		mm.verificaPrimo(valorPrimo);
 		
 		sc.close();
 	}
--- resolucaoListasJavaPOO/src/lista01/questao9/MyMath.java
+++ resolucaoListasJavaPOO/src/lista01/questao9/MyMath.java
@@ -15,4 +15,31 @@ public class MyMath {
 			return fat;
 		}
 	}
+	
+	public void maiorValor(float valorUm, float valorDois) {
+		if(valorUm > valorDois)
+			System.out.println("Esse é o maior valor: " + valorUm);
+		else
+			System.out.println("Esse é o maior valor: " + valorDois);
+	}
+	
+	public boolean verificaPar(int valorPar){
+		if(valorPar % 2 == 0)
+			return true;
+		else 
+			return false;
+	}
+	
+	public void verificaPrimo(int valorPrimo) {
+		int contDivisores = 0;
+		
+		for(int i = 1; i <= valorPrimo; i++) {
+			if(valorPrimo % i == 0)
+				contDivisores++;
+		}
+		if (contDivisores == 2)
+			System.out.println("O número digitado é primo!");
+		else
+			System.out.println("O número digitado não é primo!");
+	}
 }
