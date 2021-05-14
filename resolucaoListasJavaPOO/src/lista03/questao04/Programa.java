b0eb40c3a080d8bd20ec0073b4ee143901d1efe8
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-05-14 16:45:51 -0300

    lista03.questao04

---
+++ resolucaoListasJavaPOO/src/lista03/questao04/Programa.java
@@ -0,0 +1,11 @@
+package lista03.questao04;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		CalculadoraDeInvestimentoFinanceiro calculadora = new CalculadoraDeInvestimentoFinanceiro();
+		calculadora.entradaDadosEResultado();
+
+	}
+}
