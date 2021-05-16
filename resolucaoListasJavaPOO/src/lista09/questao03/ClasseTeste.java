68c96908aa24364fd1b08e3a286a567c4aa80fda
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-05-16 03:40:22 -0300

    lista09.questao03

---
+++ resolucaoListasJavaPOO/src/lista09/questao03/ClasseTeste.java
@@ -0,0 +1,16 @@
+package lista09.questao03;
+
+public class ClasseTeste implements ConstantesMatematicas {
+	
+	public double raizQuadradaDe2() {
+		return raizDe2;
+	}
+	
+	public double raizQuadradaDe3() {
+		return raizDe3;
+	}
+
+	public double raizQuadradaDe5() {
+		return raizDe5;
+	}
+}
---
+++ resolucaoListasJavaPOO/src/lista09/questao03/ConstantesMatematicas.java
@@ -0,0 +1,12 @@
+package lista09.questao03;
+
+public interface ConstantesMatematicas {
+	//raiz quadrada de 2
+	public static final double raizDe2 = 1.4142135623730950488;
+	//raiz quadrada de 3
+	public static final double raizDe3 = 1.7320508075688772935;
+	//raiz quadrada de 5
+	public static final double raizDe5 = 2.2360679774997896964;
+	//raiz quadrada de 6: podemos usar as constantes já definidas
+	public static final double raizDe6 = raizDe2*raizDe3;
+}
